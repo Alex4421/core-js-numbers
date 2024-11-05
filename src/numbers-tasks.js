1b4d93769c2b5116312c57000f32b5c71711ce6d
Alex4421 <cherevko.alex96@gmail.com>
2024-11-05 12:13:17 +0300

    fix: js code

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -605,8 +605,8 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -1, 1 => -1 | 0 | 1
  */
 function getRandomInteger(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  // min = Math.ceil(min);
+  // max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
